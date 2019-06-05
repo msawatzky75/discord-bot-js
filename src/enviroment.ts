9a950c62aa682b5aa8c368d7ce00d33d39de6cfd
Matthew Sawatzky <34319870+msawatzky75@users.noreply.github.com>
2019-06-05 09:38:18 -0500

    added postgres

--- src/enviroment.ts
+++
@@ -1,9 +0,0 @@
-import * as dotenv from "dotenv";
-
-dotenv.config({ path: `${__dirname}/../.env` });
-
-export const TOKEN = process.env.TOKEN;
-export const PREFIX = process.env.PREFIX;
-export const TARGET = process.env.TARGET;
-export const MONGOURL = process.env.MONGOURL;
-export const MONGODB = process.env.MONGODB;
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
 import debug from 'debug';
 import {Client} from 'discord.js';
-import {TOKEN, PREFIX, MONGOURL, MONGODB} from './enviroment';
+import * as dotenv from "dotenv";
+import {Client as PgClient} from 'pg';
 import Remind from './commands/remind';
 // import SetNickname from './commands/setNickname';
 
+dotenv.config({ path: `${__dirname}/../.env` });
 const d = debug('bot.src.index');
 const client = new Client();
-let db;
+const pgclient = new PgClient({
+	connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+});
 
-Promise.all([client.login(TOKEN)]).then((values) => {
-	db = values[0];
+Promise.all([client.login(process.env.TOKEN), pgclient.connect()]).then((values) => {
+	d('Connected to discord and postgres');
+	d(values);
+}).catch((errors) => {
+	d("error:", errors);
 });
 
 client.on('ready', () => {
@@ -17,7 +24,7 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-	if (msg.content.startsWith(PREFIX)) {
+	if (msg.content.startsWith(process.env.PREFIX)) {
 		const args: string[] = msg.content.split(' ');
 		const command: string = args.shift().substring(1);
 
@@ -37,5 +44,3 @@ client.on('message', msg => {
 		}
 	}
 });
-
-
