cfcb7f11740b29de5a4b0d2dfccf35e24cdeeafb
Matthew Sawatzky <34319870+msawatzky75@users.noreply.github.com>
2019-06-10 10:53:17 -0500

    changed .env structure

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,7 @@ import Remind from './commands/remind';
 dotenv.config({ path: `${__dirname}/../.env` });
 const d = debug('bot.src.index');
 const client = new Client();
-const pgclient = new PGClient({
-	connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
-});
+const pgclient = new PGClient({connectionString: process.env.DATABASE_URL});
 
 function loadReminders() {
 	pgclient.query({
